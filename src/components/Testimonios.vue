94ebe28d7f0d91d94aacd044fe1bcc7707e9c4d3
LuisLaiton <felipelaiton0@gmail.com>
2022-12-13 16:49:04 -0500

    Configurando imagenes

--- src/components/Testimonios.vue
+++ src/components/Testimonios.vue
@@ -29,7 +29,7 @@ function mostrarTestimonios() {
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 <div v-if="buscar != ''" class="col">
                     <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="https://placeholder.com/400px" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
@@ -38,7 +38,7 @@ function mostrarTestimonios() {
                     </div>
                 </div>
                 <div v-if="buscar == ''" class="card">
-                    <img src="..." class="card-img-top img_card" alt="...">
+                    <img src="https://via.placeholder.com/400" class="card-img-top img_card" alt="...">
                     <div class="card-body">
                         <h5 class="card-title placeholder-glow">
                             <span class="placeholder col-6"></span>
