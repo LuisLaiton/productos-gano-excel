ec81a2e69421f6f18d5db15ea16a8a18ed8087b5
LuisLaiton <50175115+LuisLaiton@users.noreply.github.com>
2023-06-22 21:02:43 -0500

    Ajuste del header

--- JS/app.js
+++ JS/app.js
@@ -36,6 +36,13 @@ fetch('https://raw.githubusercontent.com/LuisLaiton/productos-gano-excel/basica/
 // Función para desplegar/ocultar menú
 function desplegar_menu() {
     $MENU_ENLACES.classList.toggle("navBar__enlaces--desplegable");
+
+    $BTN_NAVBAR.querySelector("svg").remove()
+    if ($MENU_ENLACES.classList.contains("navBar__enlaces--desplegable")) {
+        $BTN_NAVBAR.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="M208.49 191.51a12 12 0 0 1-17 17L128 145l-63.51 63.49a12 12 0 0 1-17-17L111 128L47.51 64.49a12 12 0 0 1 17-17L128 111l63.51-63.52a12 12 0 0 1 17 17L145 128Z"/></svg>'
+    } else {
+        $BTN_NAVBAR.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor"d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" /></svg>'
+    }
 }
 
 // Función para verificar y ocultar menú al hacer clic en un enlace
